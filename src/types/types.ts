58231545ccd261b69d5325e3bbadbd5300a3ee84
satyajit444777 <satyajit.senapati@nettantra.net>
2024-07-28 01:16:39 +0530

    added skeleton loader

--- src/types/types.ts
+++ src/types/types.ts
@@ -21,3 +21,10 @@ export interface ButtonProps {
   type?: "button" | "submit" | "reset"; // Default to 'button' if not provided
   onClick?: () => void;
 }
+
+export interface SkeletonLoaderProps {
+  type: "text" | "rectangle" | "circle";
+  width?: string;
+  height?: string;
+  borderRadius?: string;
+}
---
+++ src/ui/Skeleton.tsx
@@ -0,0 +1,30 @@
+// SkeletonLoader.tsx
+import React from "react";
+
+import { SkeletonLoaderProps } from "@/types/types";
+
+const SkeletonLoader: React.FC<SkeletonLoaderProps> = ({
+  type,
+  width = "w-16",
+  height = "h-4",
+  borderRadius = "rounded",
+}) => {
+  // Determine the base class
+  const baseClasses = "bg-gray-200 animate-pulse";
+
+  // Determine type-specific classes for text || rectangle || circle
+  let typeClasses = "";
+  if (type === "text") {
+    typeClasses = `w-${width} h-${height}`;
+  } else if (type === "rectangle") {
+    typeClasses = `w-${width} h-${height || "32"}`;
+  } else if (type === "circle") {
+    typeClasses = `w-${width} h-${height || width}`;
+  }
+
+  const combinedClasses = `${baseClasses} ${typeClasses} ${borderRadius}`;
+
+  return <div className={combinedClasses} />;
+};
+
+export default SkeletonLoader;
