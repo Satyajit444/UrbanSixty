a00d3c9a5a4ad91bf5b5841757edee2cfa816bb7
satyajit444777 <satyajit.senapati@nettantra.net>
2024-07-28 00:16:30 +0530

    updated environment and config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    // Product Endpoints
+    PRODUCTS_API: "https://fakestoreapi.com/products",
+    PRODUCT_BY_ID_API: "https://fakestoreapi.com/products/",
+    PRODUCT_CATEGORIES_API: "https://fakestoreapi.com/products/categories",
+    PRODUCTS_BY_CATEGORY_API: "https://fakestoreapi.com/products/category/",
+
+    // Cart Endpoints
+    CARTS_API: "https://fakestoreapi.com/carts",
+    CART_BY_ID_API: "https://fakestoreapi.com/carts/",
+
+    // User Endpoints
+    USERS_API: "https://fakestoreapi.com/users",
+    USER_BY_ID_API: "https://fakestoreapi.com/users/",
+  },
+};
 
 export default nextConfig;
